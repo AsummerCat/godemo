ed388f711f9c3c1e44ef852c676730bdfebaeb2d
583188551 <583188551@qq.com>
2022-09-26 21:59:33 +0800

    基础main函数

--- day1/main.go
+++ day1/main.go
@@ -2,13 +2,15 @@ package main
 
 import "fmt"
 
-// 批量声明
+// 变量批量声明
 var (
 	name string
 	age  int
 	isOk bool
 )
 
+// 主函数执行
+// 函数外 只能设置标识符(变量,常量,函数,类型)的声明
 func main() {
 	fmt.Println("hello Word")
 	name = "小明"
