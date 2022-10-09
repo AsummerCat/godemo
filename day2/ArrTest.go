c6282544b860b1e16dfa7e02957d94bea7afcd83
chenxiaocheng <583188551@qq.com>
2022-10-09 14:06:50 +0800

    数组

---
+++ day2/ArrTest.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n [10]int /* n 是一个长度为 10 的数组 */
+	var i, j int
+
+	/* 为数组 n 初始化元素 */
+	for i = 0; i < 10; i++ {
+		n[i] = i + 100 /* 设置元素为 i + 100 */
+	}
+
+	/* 输出每个数组元素的值 */
+	for j = 0; j < 10; j++ {
+		fmt.Printf("Element[%d] = %d\n", j, n[j])
+	}
+
+	/*二维数组*/
+	// 创建二维数组
+	sites := [2][2]string{}
+
+	// 向二维数组添加元素
+	sites[0][0] = "Google"
+	sites[0][1] = "Runoob"
+	sites[1][0] = "Taobao"
+	sites[1][1] = "Weibo"
+
+	// 显示结果
+	fmt.Println(sites)
+}
--- day2/TypeTest.go
+++ day2/TypeTest.go
@@ -7,6 +7,13 @@ type Circle struct {
 	radius float64
 }
 
+type Books struct {
+	title   string
+	author  string
+	subject string
+	book_id int
+}
+
 // 该 method 属于 Circle 类型对象中的方法
 func (c Circle) getArea() float64 {
 	//c.radius 即为 Circle 类型对象中的属性
@@ -17,4 +24,7 @@ func main() {
 	var c1 Circle
 	c1.radius = 10.00
 	fmt.Println("圆的面积 = ", c1.getArea())
+
+	// 创建一个新的结构体
+	fmt.Println(Books{"Go 语言", "www.runoob.com", "Go 语言教程", 6495407})
 }
