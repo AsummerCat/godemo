5ed5ac3b801ee7b73a953103212b84c184fd7d1d
583188551 <583188551@qq.com>
2022-10-11 22:21:56 +0800

    json序列化

---
+++ day2/JsonTest.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+/*
+*
+注意字段大写 才能在别的包中查看,所以json序列化的时候需要变成大写
+*/
+type Person struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	person := Person{
+		Name: "小明",
+		Age:  100,
+	}
+	//序列化
+	marshal, err := json.Marshal(person)
+	if err != nil {
+		fmt.Println("序列化失败:%s", err)
+	}
+
+	str := string(marshal)
+	fmt.Println(string(marshal))
+
+	//反序列化
+	var p2 Person
+	json.Unmarshal([]byte(str), &p2) //传指针修改P2的赋值
+	fmt.Println(p2)
+}
